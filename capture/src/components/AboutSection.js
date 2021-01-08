aa71a6907254a4744824b907205de62897fd34f7
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-01-08 12:38:27 -0600

    5. Styled Components for Services and FAQ sections

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import home1 from '../img/home1.png';
 //Styled
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import { Layout, Description, Image, Hide } from '../styles';
 
 const AboutSection = () => {
 	return (
-		<About>
+		<Layout>
 			<Description>
 				<div className='title'>
 					<Hide>
@@ -29,40 +30,8 @@ const AboutSection = () => {
 			<Image>
 				<img src={home1} alt='camera man' />
 			</Image>
-		</About>
+		</Layout>
 	);
 };
-
-//Styled Components
-const About = styled.div`
-	min-height: 90vh;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 5rem 10rem;
-	color: white;
-`;
-
-const Description = styled.div`
-	flex: 1;
-	padding-right: 5rem;
-	h2 {
-		font-weight: lighter;
-	}
-`;
-
-const Image = styled.div`
-	flex: 1;
-	overflow: hidden;
-	img {
-		width: 100%;
-		height: 80vh;
-		object-fit: cover;
-	}
-`;
-
-const Hide = styled.div`
-	overflow: hidden;
-`;
 
 export default AboutSection;
--- capture/src/components/FaqSection.js
+++ capture/src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Layout } from '../styles';
 
 const FaqSection = () => {
 	return (
-		<div className='faq'>
+		<Faq>
 			<h2>
 				Any Questions? <span>FAQ</span>
 			</h2>
@@ -15,6 +17,7 @@ const FaqSection = () => {
 						nisi nisl. Praesent vitae ante rhoncus, ornare orci ut, condimentum.
 					</p>
 				</div>
+				<div className='faq-line'></div>
 			</div>
 
 			<div className='question'>
@@ -27,6 +30,7 @@ const FaqSection = () => {
 						ipsum. Fusce vestibulum lacus sit.
 					</p>
 				</div>
+				<div className='faq-line'></div>
 			</div>
 
 			<div className='question'>
@@ -38,6 +42,7 @@ const FaqSection = () => {
 						Phasellus pulvinar, tellus id condimentum.
 					</p>
 				</div>
+				<div className='faq-line'></div>
 			</div>
 
 			<div className='question'>
@@ -49,9 +54,37 @@ const FaqSection = () => {
 						pretium. Fusce varius mauris et dui tempus, quis.
 					</p>
 				</div>
+				<div className='faq-line'></div>
 			</div>
-		</div>
+		</Faq>
 	);
 };
+
+const Faq = styled(Layout)`
+	display: block;
+	span {
+		display: block;
+	}
+	h2 {
+		padding-bottom: 2rem;
+		font-weight: lighter;
+	}
+	.faq-line {
+		background: #cccccc;
+		height: 0.2rem;
+		margin: 2rem 0rem;
+		width: 100%;
+	}
+	.question {
+		padding: 3rem 0rem;
+		cursor: pointer;
+	}
+	.answer{
+		padding: 2rem 0rem:
+		p{
+			padding: 1rem 0rem;
+		}
+	}
+`;
 
 export default FaqSection;
