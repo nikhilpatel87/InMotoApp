1fd6e1550e29cea3abbb9b6cb933f42138199b2f
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-01-13 14:38:53 -0600

    11. Add AboutSection animation

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -4,6 +4,7 @@ import home1 from '../img/home1.png';
 import { Layout, Description, Image, Hide } from '../styles';
 //import framer motion
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
 
 const AboutSection = () => {
 	return (
@@ -11,25 +12,25 @@ const AboutSection = () => {
 			<Description>
 				<motion.div>
 					<Hide>
-						<motion.h2>We work to make</motion.h2>
+						<motion.h2 variants={titleAnim}>We work to make</motion.h2>
 					</Hide>
 					<Hide>
-						<motion.h2>
+						<motion.h2 variants={titleAnim}>
 							your <span>dreams</span> come
 						</motion.h2>
 					</Hide>
 					<Hide>
-						<motion.h2>true.</motion.h2>
+						<motion.h2 variants={titleAnim}>true.</motion.h2>
 					</Hide>
 				</motion.div>
-				<p>
+				<motion.p variants={fade}>
 					Contact us for any photography or videography ideas you may have. We
 					have professionals ready to make your ideas a reality!
-				</p>
-				<button>Contact Us</button>
+				</motion.p>
+				<motion.button variants={fade}>Contact Us</motion.button>
 			</Description>
 			<Image>
-				<img src={home1} alt='camera man' />
+				<motion.img src={home1} alt='camera man' variants={photoAnim} />
 			</Image>
 		</Layout>
 	);
