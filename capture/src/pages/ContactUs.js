4a3b34692d089e672ceb16c5232c42ea9577e210
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-01-15 17:05:36 -0600

    16. Add Contact Us Animation

--- capture/src/pages/ContactUs.js
+++ capture/src/pages/ContactUs.js
@@ -1,18 +1,77 @@
 import React from 'react';
 //Animation
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, titleAnim } from '../animation';
+
+import styled from 'styled-components';
 
 const ContactUs = () => {
 	return (
-		<motion.div
+		<ContactStyle
 			variants={pageAnimation}
 			initial='hidden'
 			animate='show'
-			exit='exit'>
-			<h1>Our Contact Info</h1>
-		</motion.div>
+			exit='exit'
+			style={{ background: '#fff' }}>
+			<Title>
+				<Hide>
+					<motion.h2 variants={titleAnim}>Get In Touch.</motion.h2>
+				</Hide>
+			</Title>
+			<div>
+				<Hide>
+					<Social variants={titleAnim}>
+						<Circle />
+						<h2>Send Us A Message</h2>
+					</Social>
+				</Hide>
+
+				<Hide>
+					<Social variants={titleAnim}>
+						<Circle />
+						<h2>Tweet Us</h2>
+					</Social>
+				</Hide>
+
+				<Hide>
+					<Social variants={titleAnim}>
+						<Circle />
+						<h2>Check Out Our Instagram</h2>
+					</Social>
+				</Hide>
+			</div>
+		</ContactStyle>
 	);
 };
+
+const ContactStyle = styled(motion.div)`
+	padding: 5rem 10rem;
+	color: #353535;
+	min-height: 90vh;
+`;
+
+const Title = styled.div`
+	margin-bottom: 4rem;
+	color: black;
+`;
+
+const Hide = styled.div`
+	overflow: hidden;
+`;
+
+const Circle = styled.div`
+	border-radius: 50%;
+	width: 3rem;
+	height 3rem;
+	background: #23d997;
+`;
+
+const Social = styled(motion.div)`
+	display: flex;
+	align-items: center;
+	h2 {
+		margin: 2rem;
+	}
+`;
 
 export default ContactUs;
